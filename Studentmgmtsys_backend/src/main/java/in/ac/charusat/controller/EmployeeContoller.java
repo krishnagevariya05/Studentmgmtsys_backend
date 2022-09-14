42254518bc8a1ada287e917a54a7396daae30401
krishnagevariya05 <88083861+krishnagevariya05@users.noreply.github.com>
2022-09-14 00:24:29 -0700

    Add files via upload

--- Studentmgmtsys_backend/src/main/java/in/ac/charusat/controller/EmployeeContoller.java
+++ Studentmgmtsys_backend/src/main/java/in/ac/charusat/controller/EmployeeContoller.java
@@ -1,12 +1,12 @@
 package in.ac.charusat.controller;
 
-import in.ac.charusat.model.Employee20it051;
+import in.ac.charusat.model.Employee20it036;
 import in.ac.charusat.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
 public class EmployeeContoller
@@ -14,30 +14,30 @@ public class EmployeeContoller
    @Autowired
     EmployeeRepository employeeRepository;
     @GetMapping("listEmployees")
-    public List<Employee20it051> getAllStudents()
+    public List<Employee20it036> getAllStudents()
     {
         return employeeRepository.findAll();
     }
     @GetMapping("/emp/{id}")
-    public Employee20it051 getStudent(@PathVariable Integer id) {
+    public Employee20it036 getStudent(@PathVariable Integer id) {
         return employeeRepository.findById(id).get();
     }
     @PostMapping("/employee")
-    public List<Employee20it051> addStudent(@RequestBody Employee20it051 emp)
+    public List<Employee20it036> addStudent(@RequestBody Employee20it036 emp)
     {
         employeeRepository.save(emp);
         return employeeRepository.findAll();
     }
     @DeleteMapping("/delete/{id}")
-    public List<Employee20it051> deleteEmployee(@PathVariable Integer id)
+    public List<Employee20it036> deleteEmployee(@PathVariable Integer id)
     {
         employeeRepository.delete(employeeRepository.findById(id).get());
         return employeeRepository.findAll();
     }
     @PutMapping("/update/{id}")
-    public List<Employee20it051> updateEmployee(@RequestBody Employee20it051 emp ,@PathVariable Integer id)
+    public List<Employee20it036> updateEmployee(@RequestBody Employee20it036 emp , @PathVariable Integer id)
     {
-        Employee20it051 empobj=employeeRepository.findById(id).get();
+        Employee20it036 empobj=employeeRepository.findById(id).get();
         empobj.setName(emp.getName());
         empobj.setAddress(emp.getAddress());
         employeeRepository.save(empobj);
